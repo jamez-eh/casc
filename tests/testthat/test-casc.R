7bc3c4e6594bcbac90dcb47804400604ad5b3b46
James <jamesedhopkins@gmail.com>
2019-02-04 20:07:58 +0000

    changed documentation

--- tests/testthat/test-casc.R
+++ tests/testthat/test-casc.R
@@ -1,9 +1,8 @@
 context("test-casc")
 
 test_that("cascer returns correct types", {
+  library(SingleCellExperiment)
   test_sce <- data(test_sce)
-
-
   casc_obj <- cascer(test_sce, sce$sc3_3_clusters)
   expect_is(casc_obj, "casc")
 })
