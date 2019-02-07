6984902254e3ee6d6a2d381faaa1b80fe36ca306
James Hopkins <jamesedhopkins@gmail.com>
2019-02-07 23:01:44 +0000

    updated print for casc object, made cascer accept multiple clusters

--- R/utils.R
+++ R/utils.R
@@ -1,52 +1,64 @@
 
 
 variableGenes <- function(sce, marker_num){
-
+  
   if(dim(sce)[1] > marker_num) {
     vars <- rowVars(as.matrix(logcounts(sce)))
     genes <- rownames(sce)
     df <- data.frame(genes, vars, stringsAsFactors = FALSE)
     df <- df[order(df$vars, decreasing=TRUE), ][1:marker_num,]
-
+    
     return(sce[df$genes,])
-
-    # topgenes <- arrange(df, desc(vars)) %>%
-    #   head(n = numGenes) %>%
-    #   .$genes
-    #
-    # return(sce[topgenes,])
+    
   }
   else {
     return(sce)
   }
-
+  
 }
 
 
 sampleSplit <- function(sce, marker_num=2000) {
-
+  
   if(dim(sce)[1] > marker_num) {
     sce<- variableGenes(sce, marker_num)
   }
-
-  training_size <- ceiling(0.5 * length(colnames(sce)))
-
-  training <- sce[, sample(colnames(sce), training_size)]
+  
+  trainingSize <- ceiling(0.5 * length(colnames(sce)))
+  
+  training <- sce[, sample(colnames(sce), trainingSize)]
   testing <- sce[, setdiff(colnames(sce), colnames(training))]
-  train_clus <- as.data.frame(as.character(training$clusters))
-  test_clus <- as.data.frame(as.character(testing$clusters))
-
-
-  training_matrix <- t(assay(training, "logcounts"))
-  testing_matrix <- t(assay(testing, "logcounts"))
-  training_label_df <- as.data.frame(training$clusters)
-  training_labels <- as.data.frame(training$clusters)$`training$clusters`
-  testing_labels <- as.data.frame(testing$clusters)$`testing$clusters`
-
-  return(list(training_matrix, training_labels, testing_matrix, testing_labels))
-
+  
+  
+  return(list(training,testing))
+  
 }
 
+single_casc <- function(cluster_name, dataSplits){
+  trainY <- chartr("123456789", "ABCDEFGHI", colData(dataSplits[[1]])[cluster_name][[1]])
+  
+  trcntrl <- trainControl(method="cv", number=5, returnResamp="all",
+                          classProbs=TRUE,
+                          savePredictions = TRUE)
+  
+  fit <- train(t(logcounts(dataSplits[[1]])), trainY, method = "glmnet",
+               trControl=trcntrl, metric="Accuracy",
+               tuneGrid=expand.grid(alpha=1,
+                                    lambda=seq(0.001, 0.1, by=0.001)))
+  
+  
+  probs <- predict(fit, newdata=t(logcounts(dataSplits[[2]])), type="prob")
+  
+  classes <- chartr("ABCDEFGHI", "123456789", predict(fit, newdata=t(logcounts(dataSplits[[2]])), type="raw")) %>%
+    as.numeric()
+  
+  auc <- as.numeric(pROC::auc(multiclass.roc(as.numeric(classes), as.numeric(colData(dataSplits[[2]])[cluster_name][[1]]))))
+  
+  obj <- list(predicted_classes=classes, auc=auc, response= probs, truths=colData(dataSplits[[2]])[cluster_name][[1]] )
+  
+  class(obj) <- "casc"
+  obj
+}
 
 #' Sample, Train, and Predict logistic regression model using singleCellExperiment and glmnet
 #'
@@ -56,40 +68,58 @@ sampleSplit <- function(sce, marker_num=2000) {
 #' @return A casc object with predicted classes, auc, response, and truths
 #'
 #' @export
-cascer <- function(sce, clusters){
-
+cascer <- function(sce, ...){
+  
+  clusters <- list(...)
+  
+  k <- 0
+  l <- list()
+  for (c in clusters){
+    k <- k + 1
+    cluster_name <- paste0("casc_clusters_", k)
+    colData(sce)[cluster_name] <- c
+    l[k] <- cluster_name
+  }
   sce$clusters <- clusters
-
-  #split data
-  data_splits <- sampleSplit(sce)
-  train_y <- chartr("123456789", "ABCDEFGHI", data_splits[[2]])
-  test_y <- chartr("123456789", "ABCDEFGHI", data_splits[[4]])
-
-  #train glm
-  trcntrl <- trainControl(method="cv", number=5, returnResamp="all",
-               classProbs=TRUE,
-               savePredictions = TRUE)
-
-  fit <- train(data_splits[[1]], train_y, method = "glmnet",
-                               trControl = trcntrl ,metric = "Accuracy",
-                               tuneGrid = expand.grid(alpha = 1,
-                                                      lambda = seq(0.001,0.1,by = 0.001)))
-
-
-
-  probs <- predict(fit, newdata = data_splits[[3]], type = "prob")
-
-  classes <- chartr("ABCDEFGHI", "123456789", predict(fit, newdata = data_splits[[3]], type = "raw")) %>%
-    as.numeric()
-
-  auc <- as.numeric(pROC::auc(multiclass.roc(as.numeric(classes), as.numeric(data_splits[[4]]))))
-
-  obj <- list(predicted_classes=classes, auc=auc, response= probs, truths=data_splits[[4]])
-
-  class(obj) <- "casc"
-
-  return(obj)
+  
+  dataSplits <- sampleSplit(sce)
+  cascs <- lapply(l, single_casc, dataSplits=dataSplits)
+  
+  names(cascs) <- l
+  
+  class(cascs) <- "casc_list"
+  
+  cascs
+  
 }
+
+
+#' Print a \code{cellassign} fit
+#'
+#' @param x An object of class \code{casc}
+#' @param ... Additional arguments (unused)
+#'
+#'
+#' @return Prints a structured representation of the \code{casc}
+#
+#' @export
+print.casc <- function(x, ...) {
+  N <- length(levels(x$truths))
+  
+  cat(glue::glue("A casc object for {N} provided classes
+                 ",
+                 "Call x$predicted_classes to access predictions of class
+                 ",
+                 "Call x$auc to access area under the curve
+                 ",
+                 "Call x$response to access prediction response
+                 ",
+                 "Call x$truths to access original classes\n\n"))
+}
+
+
+
+
 
 #' Sample, Train, and Predict logistic regression model using singleCellExperiment and glmnet
 #'
@@ -101,11 +131,9 @@ cascer <- function(sce, clusters){
 multROC <- function(casc){
   truths <- casc$truths
   response <-casc$response
-  #create list of rocs
   if(length(levels(truths) > 2)) {
     roc_l <- list()
     for(i in as.numeric(levels(as.factor(truths)))) {
-      #binarize each level
       single_t <- ifelse(truths == i, 1, 0)
       roc_l[i] <- list(pROC::roc(single_t, response[,i]))
     }
@@ -114,7 +142,7 @@ multROC <- function(casc){
     roc_l[1] <- list(pROC::roc(truths, response[,1]))
   }
 
-  ggroc(roc_l) + guides(fill=guide_legend(title="Cluster"))
+  pROC::ggroc(roc_l) + theme(legend.title=element_blank())
 }
 
 
