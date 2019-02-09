bb16572a25bdf4b15492a6ffa08e2a90da934130
James Hopkins <jamesedhopkins@gmail.com>
2019-02-09 00:19:29 +0000

    added documentation and a plot function for auc scatter

---
+++ R/data_docs.R
@@ -0,0 +1,19 @@
+#' casc objects within a casc_list object for testing purposes
+#'
+#' @docType data
+#' @keywords datasets
+#' @name test_casc_list
+#' @usage data(test_casc_list)
+#' @format A casc_list obj with two casc objects within
+NULL
+
+
+
+#' a SingleCellExpermient object for testing purposes
+#'
+#' @docType data
+#' @keywords datasets
+#' @name test_sce
+#' @usage data(test_sce)
+#' @format SingleCellExperiment
+NULL
