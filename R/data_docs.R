b50fac4fff057f1b383e81c0f743df1af06bdabe
root <root@krcds.0ltngohc5k0eze10ob3ctgnamd.ux.internal.cloudapp.net>
2019-03-06 15:02:34 -0800

    added dataset for vignette and more text to vignette

--- R/data_docs.R
+++ R/data_docs.R
@@ -17,3 +17,13 @@ NULL
 #' @usage data(test_sce)
 #' @format SingleCellExperiment
 NULL
+
+
+#' a simulated SingleCellExpermient object for vignette with 1000 cells and 500 genes. 10 artificial clusters contain 20 marker genes a piece.
+#'
+#' @docType data
+#' @keywords datasets
+#' @name sce_sim
+#' @usage data(sce_sim)
+#' @format SingleCellExperiment
+NULL
